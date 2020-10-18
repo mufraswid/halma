d1f97b414b2d39fb47f555414397329aadb96679
13518073 Muhammad Cisco Zulfikar <ciscozulfikar25@gmail.com>
2020-10-19 00:47:50 +0700

    added index.js

---
+++ src/index.js
@@ -0,0 +1,5 @@
+localStorage.mode("mode")
+localStorage.tlimit("timeLimit", value)
+localStorage.bsize("boardSize", value)
+localStorage.p1color("p1color",value)
+localStorage.p2color("p2color", value)

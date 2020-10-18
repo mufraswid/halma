cd673e15c6cfc4f983b71febee5066c6ad173bf3
unknown <farismuhammad8201@gmail.com>
2020-10-18 21:38:34 +0700

    halma AI

--- src/engine/ai.js
+++ src/engine/ai.js
@@ -8,101 +8,143 @@ class HalmaAI {
         this.player = player;
     }
 
-
-    getAllMoves(coord, board) {
-    	let moves = []
-    	let down = coord.GetY();
-    	let up = coord.GetY();
-    	let right = coord.GetX();
-    	let left = coord.GetY();
-    	while (down != 0) {
-    		let action = new Action(this.player, coord, new Coordinate(coord.GetX(), down - 1))
-    		if action.isLegal(board) {
-    			moves.push(action)
-    			break;
-    		}
-    		down--
-    	}
-    	while (up != board.getSize()) {
-    		action = new Action(this.player, coord, new Coordinate(coord.GetX(), up + 1))
-    		if action.isLegal(board) {
-    			moves.push(action)
-    			break;
-    		}
-    		up++
-    	}
-    	while (right != board.getSize()) {
-    		action = new Action(this.player, coord, new Coordinate(right + 1, coord.GetY()))
-    		if action.isLegal(board) {
-    			moves.push(action)
-    			break;
-    		}
-    		right++
-    	}
-    	while (left != 0) {
-    		action = new Action(this.player, coord, new Coordinate(left - 1, coord.GetY()))
-    		if action.isLegal(board) {
-    			moves.push(action)
-    			break;
-    		}
-    		left--
-    	}
-    	down = coord.GetY();
-    	up = coord.GetY();
-    	right = coord.Getx();
-    	left = coord.GetY();
-    	while (down != 0 && right != board.getSize()) {
-    		action = new Action(this.player, coord, new Coordinate(right + 1, down - 1))
-    		if action.isLegal(board) {
-    			moves.push(action)
-    			break;
-    		}
-    		down--
-    		right++
-    	}
-    	while (up != board.getSize() && left != 0) {
-    		action = new Action(this.player, coord, new Coordinate(left - 1, up + 1))
-    		if action.isLegal(board) {
-    			moves.push(action)
-    			break;
-    		}
-    		up++
-    		left--
-    	}
-    	down = coord.GetY();
-    	up = coord.GetY();
-    	right = coord.Getx();
-    	left = coord.GetY();
-    	while (down != 0 && left != 0) {
-    		action = new Action(this.player, coord, new Coordinate(left - 1, down - 1))
-    		if action.isLegal(board) {
-    			moves.push(action)
-    			break;
-    		}
-    		down--
-    		left--
-    	}
-    	while (up != board.getSize() && right != board.getSize()) {
-    		action = new Action(this.player, coord, new Coordinate(right + 1, up + 1))
-    		if action.isLegal(board) {
-    			moves.push(action)
-    			break;
-    		}
-    		up++
-    		right++
-    	}
-    	return moves;
+    getAllStates(coord, board) {
+    	let states = []
+        let up = new Action(this.player, coord, new Coordinate(x, y + 1));
+        if (up.isLegal()) {
+            states.push(board.copyCons().updateBoard(up))
+        }
+        else {
+            states.concat(getAllHops(up.getAfterCoord(),board))
+        }
+        let down = new Action(this.player, coord, new Coordinate(x, y - 1));
+        if (down.isLegal()) {
+            states.push(board.copyCons().updateBoard(down))
+        }
+        else {
+            states.concat(getAllHops(down.getAfterCoord(),board))
+        }
+        let right = new Action(this.player, coord, new Coordinate(x + 1, y));
+        if (right.isLegal()) {
+            states.push(board.copyCons().updateBoard(right))
+        }
+        else {
+            states.concat(getAllHops(right.getAfterCoord(),board))
+        }
+        let left = new Action(this.player, coord, new Coordinate(x - 1, y));
+        if (left.isLegal()) {
+            states.push(board.copyCons().updateBoard(left))
+        }
+        else {
+            states.concat(getAllHops(left.getAfterCoord(),board))
+        }
+        let upright = new Action(this.player, coord, new Coordinate(x + 1, y + 1));
+        if (upright.isLegal()) {
+            states.push(board.copyCons().updateBoard(upright))
+        }
+        else {
+            states.concat(getAllHops(upright.getAfterCoord(),board))
+        }
+        let upleft = new Action(this.player, coord, new Coordinate(x - 1, y + 1));
+        if (upleft.isLegal()) {
+            states.push(board.copyCons().updateBoard(upleft))
+        }
+        else {
+            states.concat(getAllHops(upleft.getAfterCoord(),board))
+        }
+        let downright = new Action(this.player, coord, new Coordinate(x + 1, y - 1));
+        if (downright.isLegal()) {
+            states.push(board.copyCons().updateBoard(downright))
+        }
+        else {
+            states.concat(getAllHops(downright.getAfterCoord(),board))
+        }
+        let downleft = new Action(this.player, coord, new Coordinate(x - 1, y - 1));
+        if (downleft.isLegal()) {
+            states.push(board.copyCons().updateBoard(downleft))
+        }
+        else {
+            states.concat(getAllHops(downleft.getAfterCoord(),board))
+        }
+    	return states;
     }
 
-    /*getMove() {
+    getAllHops(coord, board) {
+        let states = []
+        let up = new Action(this.player, coord, new Coordinate(x, y + 2));
+        if (up.isLegal()) {
+            states.push(board.copyCons().updateBoard(up))
+        }
+        else {
+            states.concat(getAllHops(up.getAfterCoord(),board))
+        }
+        let down = new Action(this.player, coord, new Coordinate(x, y - 2));
+        if (down.isLegal()) {
+            states.push(board.copyCons().updateBoard(down))
+        }
+        else {
+            states.concat(getAllHops(down.getAfterCoord(),board))
+        }
+        let right = new Action(this.player, coord, new Coordinate(x + 2, y));
+        if (right.isLegal()) {
+            states.push(board.copyCons().updateBoard(right))
+        }
+        else {
+            states.concat(getAllHops(right.getAfterCoord(),board))
+        }
+        let left = new Action(this.player, coord, new Coordinate(x - 2, y));
+        if (left.isLegal()) {
+            states.push(board.copyCons().updateBoard(left))
+        }
+        else {
+            states.concat(getAllHops(left.getAfterCoord(),board))
+        }
+        let upright = new Action(this.player, coord, new Coordinate(x + 2, y + 1));
+        if (upright.isLegal()) {
+            states.push(board.copyCons().updateBoard(upright))
+        }
+        else {
+            states.concat(getAllHops(upright.getAfterCoord(),board))
+        }
+        let upleft = new Action(this.player, coord, new Coordinate(x - 2, y + 2));
+        if (upleft.isLegal()) {
+            states.push(board.copyCons().updateBoard(upleft))
+        }
+        else {
+            states.concat(getAllHops(upleft.getAfterCoord(),board))
+        }
+        let downright = new Action(this.player, coord, new Coordinate(x + 2, y - 2));
+        if (downright.isLegal()) {
+            states.push(board.copyCons().updateBoard(downright))
+        }
+        else {
+            states.concat(getAllHops(downright.getAfterCoord(),board))
+        }
+        let downleft = new Action(this.player, coord, new Coordinate(x - 2, y - 2));
+        if (downleft.isLegal()) {
+            states.push(board.copyCons().updateBoard(downleft))
+        }
+        else {
+            states.concat(getAllHops(downleft.getAfterCoord(),board))
+        }
+        return states;
+    }
+
+    /*getMove(algorithm) {
     	let pieces = this.memoryBoard.getPlayerPieces(player);
-    	let moves = []
-    	 for (i = 0; i < pieces.length; i++) {
-    	 	moves.concat(getAllMoves(pieces[i], this.memoryBoard))
-    	 }
-    	let depth = this.MaxDepth;
-    	while (depth != 0 && this.memoryBoard.isFinalState() != 0) {
-    		minimax(moves, depth, isMaximizing)
-    	}
+    	let states = []
+        for (i = 0; i < pieces.length; i++) {
+    	 	states.concat(getAllStates(pieces[i], this.memoryBoard))
+        }
+    	let utilities = []
+        for (j = 0; j < pieces.length; j++) {
+            utilities.push(states[j].objfunc())
+        }
+        if (algorithm == 'minimax-only') {
+            minimax(states, utilities, this.maxDepth)
+        }
+        else if (algorithm == 'minimax-localsearch') {
+            localsearch(states, utilities, this.maxDepth)
+        }
     }
 }*/
